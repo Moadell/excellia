dd31eeba791dd770434c223edb3be68cd7b6c461
Moadell <mo.adel@nu.edu.eg>
2020-07-29 03:42:51 +0200

    Updates

---
+++ precache-manifest.46803ad98927d3b854eeea09c06eddc2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee52d1b6de8225bf85e9185e8448d9f",
+    "url": "/excellia/index.html"
+  },
+  {
+    "revision": "0ed9dc4741f493eac790",
+    "url": "/excellia/static/css/main.dbaa3ec2.chunk.css"
+  },
+  {
+    "revision": "2028f0cf1804d386cdbb",
+    "url": "/excellia/static/js/2.5fbe721a.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/excellia/static/js/2.5fbe721a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed9dc4741f493eac790",
+    "url": "/excellia/static/js/main.dab97715.chunk.js"
+  },
+  {
+    "revision": "adb65156aa64270017ea",
+    "url": "/excellia/static/js/runtime-main.46bafc22.js"
+  },
+  {
+    "revision": "26322894ce6ae8200b2bd50574ecc02e",
+    "url": "/excellia/static/media/cloud.26322894.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/excellia/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "af8c475e7d8fe353c85e221f7b6cc71c",
+    "url": "/excellia/static/media/features.af8c475e.svg"
+  },
+  {
+    "revision": "095cbef334ecbba006f2cd5e12cf2c96",
+    "url": "/excellia/static/media/heart.095cbef3.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/excellia/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/excellia/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "e03402ae0ff3806efa15300871391636",
+    "url": "/excellia/static/media/link.e03402ae.png"
+  },
+  {
+    "revision": "bd00b1d2598f9a3e961edffe0c39deca",
+    "url": "/excellia/static/media/person.bd00b1d2.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/excellia/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
